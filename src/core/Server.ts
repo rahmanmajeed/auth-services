bd62f61094ccdd4f82bb1ceef24c6f294cef6293
Majedur Rahman <Majedur.Rahman@Brainstation-23.com>
2023-04-10 15:31:12 +0600

    [add] re-usable code for server.

--- src/core/Server.ts
+++ src/core/Server.ts
@@ -1,9 +1,38 @@
-class Server {
-    constructor(){
-        //
-    }
+import dotenv from "dotenv";
+import express, { Express, Request, Response } from "express";
+import { IServer } from "../interfaces/server";
 
-    async start () {
-        // server start...
-    }
+dotenv.config();
+
+class Server {
+  app: Express = express();
+  host;
+  port;
+  options;
+  server: any;
+  constructor({ host, port, options = {} }: IServer = {}) {
+    const defaultPORT = process.env.PORT;
+    this.host = host || process.env.HOST || "0.0.0.0";
+    this.port = Number(port) || Number(defaultPORT) || 3333;
+    this.options = options;
+  }
+
+  async start() {
+
+    this.app.get('/hello', (req:Request, res:Response) => {
+      res.send('Express + TypeScript Server');
+    })
+    // server start...
+    await new Promise<void>((r) => {
+      this.server = this.app.listen(this.port, this.host, () => {
+        console.log(`⚡️[server]: Server is running at ${this.host}:${this.port}`);
+        r();
+      });
+    });
+    return {
+      express: this.app,
+      server: this.server,
+    };
+  }
 }
+export default Server
