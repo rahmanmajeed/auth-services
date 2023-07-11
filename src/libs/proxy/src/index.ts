37ffa9592e4de648af43c89a82b5f02795645d8e
rahman majeed <rahmanmajeed46@gmail.com>
2023-07-12 00:04:24 +0600

    ✨ [init] proxy server

--- src/libs/proxy/src/index.ts
+++ src/libs/proxy/src/index.ts
@@ -1,3 +1,10 @@
-export function Server () {
+const http = require('http');
+const https = require('https');
+const EE = require('events');
+const url = require('url')
+
+
+
+class HttpProxyServer{
     
 }
